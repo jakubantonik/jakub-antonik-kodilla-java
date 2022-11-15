9bca8146cc791f1cc4c21c26d5f0fd3898ea2aea
Jakub Antonik <jakubantonik@icloud.com>
2022-11-15 10:36:17 +0100

    kodilla stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,12 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -15,7 +15,7 @@ public class StreamMain {
         Forum theForum = new Forum();
         Map<Integer, ForumUser>mapOfUser = theForum.getUserList().stream()
                 .filter(user -> user.getSex() == 'M')
-                .filter(user -> user.getBirthdayDate().getYear()<2004)
+                .filter(user -> ChronoUnit.YEARS.between(user.getBirthdayDate(), LocalDate.now())>=20)
                 .filter(user -> user.getNumberOfPosts()>=1)
                 .collect(Collectors.toMap(ForumUser::getIdOfUser, user -> user));
 
