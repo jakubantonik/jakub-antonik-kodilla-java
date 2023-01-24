f026f84b1b5c613f01906707e311efacc5cd7d17
jakubantonik <114752695+jakubantonik@users.noreply.github.com>
2023-01-24 17:50:32 +0100

    Add files via upload

---
+++ ISSUES.sql
@@ -0,0 +1,5 @@
+insert into kodilla_course.ISSUES (ID, ISSUELIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO) values (1, 1, 'Broken', 'Reparing desk', 3);
+insert into kodilla_course.ISSUES (ID, ISSUELIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO) values (2, 1, 'Simple task', 'Prepare document', 4);
+insert into kodilla_course.ISSUES (ID, ISSUELIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO) values (3, 2, 'Broken', 'Reparing sink', 2);
+insert into kodilla_course.ISSUES (ID, ISSUELIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO) values (4, 2, 'Task', 'Calculate sallary', 5);
+insert into kodilla_course.ISSUES (ID, ISSUELIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO) values (5, 3, 'Broken', 'Reparing table', 3);
